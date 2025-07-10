d676a86f8c24229f5fa872f83ca8b648536a03e7
jamesTautiva <tautivamolanoj@gmail.com>
2025-07-10 09:30:37 -0500

    deploy

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('bienvenido a la api de BlackAmp!');
+})
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+})
