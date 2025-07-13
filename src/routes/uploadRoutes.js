5824e46549695973b214f72eb021821f1a8515eb
jamesTautiva <tautivamolanoj@gmail.com>
2025-07-13 15:17:09 -0500

    Update uploadRoutes.js

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const uploadController = require('../controllers/uploadController');
+const {uploadFile , updateImage}=  require('../controllers/uploadController');
 const upload = require('../middleware/multer');
 const { requireAuth } = require('../middleware/auth');
 
-router.post('/:tipo/:id', requireAuth, upload.single('file'), uploadController.uploadFile);
+router.post('/:tipo/:id', requireAuth, upload.single('file'),uploadFile);
 // ✅ Ruta para actualizar imagen
 router.put('/users/:id', requireAuth, upload.single('file'), updateImage);
 
