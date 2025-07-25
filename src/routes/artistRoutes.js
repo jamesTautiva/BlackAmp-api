07adf59f9f41827ca68716aa9ea80011605a7dae
JAMES TAUTIVA <tautivamolanoj@gmail.com>
2025-07-25 10:23:00 -0500

    Update artistRoutes.js

--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -5,11 +5,13 @@ const { requireAuth } = require('../middleware/auth');
 
 router.post('/', requireAuth, controller.createArtistProfile);
 router.get('/', controller.getAllArtists);
-router.get('/:id', controller.getArtistById);
+
 router.put('/:id', requireAuth, controller.updateArtist);
 router.delete('/:id', requireAuth, controller.deleteArtist);
 router.get('/user/:userId', controller.getArtistByUserId); // Nueva ruta importante
 // Ruta para obtener usuario con datos de artista
 router.get('/user-with-artist/:userId', controller.getUserWithArtist);
+
+router.get('/:id', controller.getArtistById);
 
 module.exports = router;
