699d9507d93377432b661ea6ed872a1dbfff4431
JAMES TAUTIVA <tautivamolanoj@gmail.com>
2025-09-17 10:19:34 -0500

    Update songRoutes.js

--- src/routes/songRoutes.js
+++ src/routes/songRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/songContoller');
 const { requireAuth } = require('../middleware/auth');
 
-router.post('/', requireAuth, controller.createSong);
+router.post('/', controller.createSong);
 router.get('/', controller.getAllSongs);
 router.get('/:id', controller.getSongById);
 router.put('/:id', requireAuth, controller.updateSong);
