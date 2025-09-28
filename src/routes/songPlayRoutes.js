a0da650c531b3ceccabe3c50e88f1fc1fccd99a9
JAMES TAUTIVA <tautivamolanoj@gmail.com>
2025-09-27 20:11:52 -0500

    Update songPlayRoutes.js

--- src/routes/songPlayRoutes.js
+++ src/routes/songPlayRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const songPlayController = require("../controllers/songPlayController");
-const authMiddleware = require("../middlewares/authMiddleware");
 
 // Registrar reproducción
-router.post("/register", authMiddleware, songPlayController.registerPlay);
+router.post("/register",  songPlayController.registerPlay);
 
 // Historial de usuario
-router.get("/history", authMiddleware, songPlayController.getUserHistory);
+router.get("/history",  songPlayController.getUserHistory);
 
 module.exports = router;
